a151eda8e968fcf43a6e6065ca005af58611b2da
Selmy <146110265+Selmy44@users.noreply.github.com>
2024-09-19 11:31:02 +0200

    Add files via upload

--- Bro Code course/index.js
+++ Bro Code course/index.js
@@ -209,4 +209,24 @@ function convert(){
 
 // DICE ROLLER Program
 
+function rollDice(){
 
+    const numOfDice = document.getElementById('numOfDice').value;
+    const diceResult = document.getElementById('diceResult');
+    const diceImages = document.getElementById('diceImages');
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 5) + 1;
+        values.push(value);
+        images.push(`<img src = "/dice_images/${value}.png" alt = "Dice ${value}">`);
+    }
+
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
+}    
+
+
+
+ 
